426e14755e457b4e62ffb03baa751ebeaf3a96a0
userluckytian <tianlukang123@163.com>
2022-12-22 23:14:32 +0800

    fix：命令构建测试

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
-console.log(1121);
+// node中通过`process.argv`获取控制台输入的参数
+// console.log(process.argv);
+
+const { Command } = require('commander'); // (normal include)
+
+const program = new Command();
+
+program
+  .name('create')    // 每个文件都 有且只有一个 name 属性
+  .version('0.0.1')  // 每个文件都 有且只有一个 version 属性
+  .option('-a, --config <path>', '这个是描述信息1')   // 每个文件后可配置多个 option 属性
+  .option('-b, --config <path>', '这个是描述信息2')  // 每个文件后可配置多个 option 属性
+  .option('-c, --config <path>', '这个是描述信息3');  // 每个文件后可配置多个 option 属性
+
+
+  program.parse(process.argv);  // 类似开了一个监听！
