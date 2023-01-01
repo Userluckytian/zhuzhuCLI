8b912b5bbff3a5681c2fdbe4e7dc6caa9dc11e45
userluckytian <tianlukang123@163.com>
2023-01-01 18:57:42 +0800

    fix： 构建项目下载地址

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,11 @@
+/* eslint-disable no-undef */
 // 该文件用于存放一些常量
 
-const { version }  = require('../package.json')
-
+const { version }  = require('../package.json'); // 获取版本号
+// 构建一个临时目录
+const downLoadTempDirectory = `${process.env[process.platform === 'win32' ? 'USERPROFILE' : 'HOME']}/.template`;
+console.log(downLoadTempDirectory);
 module.export = {
-    version
+    version,
+    downLoadTempDirectory
 };
--- src/create.js
+++ src/create.js
@@ -1,20 +1,32 @@
+/**
+ * github API使用： https://blog.csdn.net/weixin_43829154/article/details/120697007
+ * https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-repository-tags
+ * 
+ * */
+
 const axios = require('axios');
 const ora = require('ora'); // 版本过高可能会报使用方式错误。降低本吧就好了，建议v3版本
 const Inquirer = require('inquirer'); // 建议v8版本
+const { downLoadTempDirectory } = require('./constants')
 
+// 引入下载代码仓库需要的依赖项（第（5）步的时候看，现在可以忽略）
+let downloadGitRepo = require('download-git-repo');
+const { promisfy } = require('promisfy');
+downloadGitRepo = promisfy(downloadGitRepo);
 
 // 获取项目模板列表
 const fetchRepoList = async () => {
     const { data } = await axios.get('https://api.github.com/users/Userluckytian/repos')
     return data;
 }
+
 // 封装loading 需要传入方法以及显示的内容
-const waiting = async (asyncfn, showLabel) => {
+const waiting = (asyncfn, showLabel) => async (...args) => {
     const spinner = ora(`${showLabel}\n`);
     spinner.start();
     // 放置获取失败！
     try {
-        const data = await asyncfn();
+        const data = await asyncfn(args);
         spinner.succeed();
         return data
     } catch (error) {
@@ -24,10 +36,25 @@ const waiting = async (asyncfn, showLabel) => {
     }
 }
 
-// 获取代码仓库的版本号 
+// 获取代码仓库的版本号
+const fetchRepoTagList = async (selRepoName) => {
+    const { data } = await axios.get(`https://api.github.com/repos/Userluckytian/${selRepoName}/tags`)
+    return data;
+}
+//  下载代码仓库
+const downLoadRepo = async (repoName, repoTag) => {
+    let api = `Userluckytian/${repoName}`;
+    if (repoTag) {
+        api += `#${repoTag}`
+    }
+    const dist = `${downLoadTempDirectory}/${repoName}`;
+    console.log(dist);
+    await downloadGitRepo(api, dist);
+    return dist; // 返回下载的最终目录
+}
 
 module.exports = async (projectName) => {
-    console.log('create 指令，要创建的项目名称为：', projectName);
+    // console.log('create 指令，要创建的项目名称为：', projectName);
     // (1) 拉取代码仓库列表(通过接口，请先安装axios)：
     // const repoList = await fetchRepoList();
     // const repoNameList = repoList.map(repoItem => repoItem.name);
@@ -52,7 +79,7 @@ module.exports = async (projectName) => {
     // }
 
     /*(1-2)优化： 封装loading效果 */
-    const repoList = await waiting(fetchRepoList, 'fetching template ...')
+    const repoList = await waiting(fetchRepoList, 'fetching template ...')();
     const repoNameList = repoList.map(repoItem => repoItem.name);
     // console.log(repoNameList);
 
@@ -60,12 +87,53 @@ module.exports = async (projectName) => {
         https://www.npmjs.com/package/inquirer
         http://t.zoukankan.com/mengfangui-p-12313440.html
      */
-    const InquirerOptions = {
+    const repoInquirerOptions = {
         name: 'selRepoName',
         type: 'list',
         message: 'place choise a template to create project \n',
         choices: repoNameList
     };
-    const { selRepoName } = await Inquirer.prompt(InquirerOptions);
-    console.log(selRepoName);
+    const { selRepoName } = await Inquirer.prompt(repoInquirerOptions);
+    // console.log(selRepoName);
+
+    // (3) 获取版本号：
+    const tagList = await waiting(fetchRepoTagList, 'fetching tags ...')(selRepoName);
+    const repotagList = tagList.map(tagItem => tagItem.name);
+    // console.log(repotagList);
+
+    // (4) 构建版本选择器
+    if (repotagList.length) {
+        const tagInquirerOptions = {
+            name: 'selTagName',
+            type: 'list',
+            message: 'place choise a template to create project \n',
+            choices: repotagList
+        };
+        const { selTagName } = await Inquirer.prompt(tagInquirerOptions);
+        console.log(selTagName);
+        // （5）下载项目模板（需要安装： download-git-repo，由于不是promise类型的，需要安装promisfy工具将其返回转为promise）
+        /*
+            download-git-repo: https://www.npmjs.com/package/download-git-repo
+            promisfy: https://www.npmjs.com/package/promisfy
+           使用：
+            let downloadGitRepo = require(download-git-repo);
+           const {promisfy} = require('promisfy');
+           downloadGitRepo = promisfy(downloadGitRepo);
+        */
+        console.log('项目下载中...');
+        console.log(`${downLoadTempDirectory}/${selRepoName}`);
+        const dist = await waiting(downLoadRepo, 'download Template ...')(selRepoName, selTagName);
+        console.log('有tag', dist);
+        // （5-1）简单的，直接拷贝到当前目录
+        // （5-2）复杂的，需要渲染和，再拷贝到当前目录
+    } else {
+        // （5）下载项目模板（需要安装： download-git-repo，由于不是promise类型的，需要安装promisfy工具将其返回转为promise）
+        console.log('项目下载中...');
+        console.log(`${downLoadTempDirectory}/${selRepoName}`);
+        const dist = await waiting(downLoadRepo, 'download Template ...')(selRepoName);
+        console.log('无tag', dist);
+        // （5-1）简单的，直接拷贝到当前目录
+        // （5-2）复杂的，需要渲染和，再拷贝到当前目录
+    }
+
 }
